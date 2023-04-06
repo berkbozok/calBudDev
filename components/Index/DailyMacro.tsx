2c1dbc736f1e43373dfe536d4a0e4176059c7f08
berkbozok <berk_bozok@live.com>
2023-04-06 00:06:48 -0400

    nav bar improved

--- components/Index/DailyMacro.tsx
+++ components/Index/DailyMacro.tsx
@@ -1,56 +1,61 @@
-import React, { useContext } from 'react'
-import Protein from '../../shared/protein'
-import Fat from '../../shared/fat'
-import Carbs from '../../shared/carbs'
-import DemoPie from '../../components/Charts/PieChart'
-import Link from 'next/link'
-import { GlobalContext, globalContextTypes } from '@/pages'
-import { Button } from 'antd'
+import React, { useContext } from "react";
+import Protein from "../../shared/protein";
+import Fat from "../../shared/fat";
+import Carbs from "../../shared/carbs";
+import DemoPie from "../../components/Charts/PieChart";
+import Link from "next/link";
+import { GlobalContext, globalContextTypes } from "@/pages";
+import { Button } from "antd";
 function DailyMacro() {
   const { fatIntake, carbIntake, proteinIntake, bmrValue }: globalContextTypes =
-    useContext(GlobalContext)
+    useContext(GlobalContext);
 
-  let totalCal = proteinIntake + carbIntake + fatIntake
-  let proteinPercentage = Math.ceil((proteinIntake / totalCal) * 100)
-  let carbsPercentage = Math.ceil((carbIntake / totalCal) * 100)
-  let fatPercentage = Math.ceil((fatIntake / totalCal) * 100)
+  let totalCal = proteinIntake + carbIntake + fatIntake;
+  let proteinPercentage = Math.ceil((proteinIntake / totalCal) * 100);
+  let carbsPercentage = Math.ceil((carbIntake / totalCal) * 100);
+  let fatPercentage = Math.ceil((fatIntake / totalCal) * 100);
   return (
-    <div className='daily-macro-title'>
-      <h2 className='right-side-title'>Your Daily Macro Goals</h2>
-      <div className='ring'>
+    <div className="daily-macro-title">
+      <h2 className="right-side-title">Your Daily Macro Goals</h2>
+      <div className="ring">
         <h3>Total Maintain</h3>
-        <h3 className='bmr'> {bmrValue} </h3>
+
+        {bmrValue !== 0 ? (
+          <h3 className="bmr"> {bmrValue} </h3>
+        ) : (
+          <h3 className="bmr-calculate"> Calculate </h3>
+        )}
         <h3> kcal</h3>
       </div>
 
       {proteinIntake !== 0 || carbIntake !== 0 || fatIntake !== 0 ? (
         <>
-          <div className='table'>
-            <div className='row'></div>
+          <div className="table">
+            <div className="row"></div>
 
-            <div className='row'>
-              <div className='cell'>
+            <div className="row">
+              <div className="cell">
                 Mild weight loss <div>(0.25 kg/week)</div>
               </div>
-              <div className='cell'>
+              <div className="cell">
                 <b>{Math.ceil(bmrValue * 0.92)}</b> <span>(92%)</span>
                 <div>Calories/day</div>
               </div>
             </div>
-            <div className='row'>
-              <div className='cell'>
+            <div className="row">
+              <div className="cell">
                 Weight loss <div>(0.5 kg/week)</div>
               </div>
-              <div className='cell'>
+              <div className="cell">
                 <b> {Math.ceil(bmrValue * 0.83)}</b> <span>(83%)</span>
                 <div>Calories/day</div>
               </div>
             </div>
-            <div className='row'>
-              <div className='cell'>
+            <div className="row">
+              <div className="cell">
                 Extreme weight loss <div>(1 kg/week)</div>
               </div>
-              <div className='cell'>
+              <div className="cell">
                 <b> {Math.ceil(bmrValue * 0.66)}</b> <span>(66%)</span>
                 <div>Calories/day</div>
               </div>
@@ -60,34 +65,34 @@ function DailyMacro() {
       ) : (
         <></>
       )}
-      <div className='daily-macro-value'>
-        <div className='macro-value-div'>
-          <div className='value-box-alignment'>
+      <div className="daily-macro-value">
+        <div className="macro-value-div">
+          <div className="value-box-alignment">
             <Protein />
-            <span className='value-title'>{proteinIntake}g</span>
+            <span className="value-title">{proteinIntake}g</span>
             <span> Protein</span>
           </div>
         </div>
-        <div className='macro-value-div'>
-          <div className='value-box-alignment'>
+        <div className="macro-value-div">
+          <div className="value-box-alignment">
             <Carbs />
-            <span className='value-title'>{carbIntake}g</span>
+            <span className="value-title">{carbIntake}g</span>
             <span> Carbs</span>
           </div>
         </div>
-        <div className='macro-value-div'>
-          <div className='value-box-alignment'>
+        <div className="macro-value-div">
+          <div className="value-box-alignment">
             <Fat />
-            <span className='value-title'>{fatIntake}g</span>
+            <span className="value-title">{fatIntake}g</span>
             <span> Fat</span>
           </div>
         </div>
       </div>
       {proteinIntake !== 0 || carbIntake !== 0 || fatIntake !== 0 ? (
         <>
-          <div className='formula-title'>Our formula for you</div>
-          <div className='pie-chart-alignment'>
-            <div className='info-panel'>
+          <div className="formula-title">Our formula for you</div>
+          <div className="pie-chart-alignment">
+            <div className="info-panel">
               If you are counting macros for bodybuilding and muscle gain, you
               will want to add overall calories to put on weight. Try this range
               of macro ratio:&nbsp;
@@ -106,16 +111,16 @@ function DailyMacro() {
         </>
       ) : (
         <>
-          <div className='replacement-title'>
+          <div className="replacement-title">
             Calculate Macros to see a detailed review
           </div>
         </>
       )}
-      <Link href='/trainers'>
-        <Button className='find-trainer'>Consult with a Trainer</Button>
+      <Link href="/trainers">
+        <Button className="find-trainer">Consult with a Trainer</Button>
       </Link>
     </div>
-  )
+  );
 }
 
-export default DailyMacro
+export default DailyMacro;
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -8,23 +8,108 @@ import {
   Modal,
   Input,
   Form,
+  Menu,
+  MenuProps,
+  theme,
+  Layout,
 } from "antd";
 import {
   CalculatorFilled,
   HeartFilled,
   PercentageOutlined,
   PieChartFilled,
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 import React, { useState, useEffect } from "react";
 import { Card } from "antd";
 import Paragraph from "antd/es/typography/Paragraph";
 import Link from "next/link";
+import Logo from "../shared/logo";
+
 const { Meta } = Card;
 
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <FileOutlined />),
+];
+
 export default function Navigation() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
     <>
-      <div className="nav-bar">
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          theme="light"
+        >
+          {/* <div
+            style={{
+              height: 32,
+              margin: 16,
+              background: "rgba(255, 255, 255, 0.2)",
+            }}
+          >
+            CalBud
+          </div> */}
+
+          <Link href="/">
+            <div className="title">
+              <div>
+                <div>
+                  <Logo />
+                </div>
+                <div>
+                  <span className={collapsed ? "hidden" : ""}>CalBud</span>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["1"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+      </Layout>
+      {/* <div className="nav-bar">
         <Link href="/">
           <div className="title">CalBud</div>
         </Link>
@@ -57,7 +142,7 @@ export default function Navigation() {
         <div className="button-div">
           <Button className="upgrade-button">Upgrade</Button>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
