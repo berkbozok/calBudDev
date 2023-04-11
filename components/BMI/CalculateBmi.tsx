58af356f543dbf96f4d26d7b2e330466dca1590f
berkbozok <berk_bozok@live.com>
2023-04-11 16:35:44 -0400

    BMI added

---
+++ components/BMI/CalculateBmi.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Modal } from "antd";
+import { PieChartFilled } from "@ant-design/icons";
+import { Card } from "antd";
+
+const { Meta } = Card;
+
+const CalculateBmi = () => {
+  return <div className="page-layout">hello</div>;
+};
+
+export default CalculateBmi;
--- src/pages/bmi.tsx
+++ src/pages/bmi.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import Navigation from "../../components/Navigation";
 import PersonalTrainerList from "../../components/Trainer/PersonalTrainerList";
+import CalculateBmi from "../../components/BMI/CalculateBmi";
 
 function Bmi() {
   return (
     <div className="navigation-side">
       <Navigation />
-      HELLOO
+      <CalculateBmi />
     </div>
   );
 }
