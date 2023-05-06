aca3ecd064c6ffbddf69a24fde94334bbb7c19bb
berkbozok <berk_bozok@live.com>
2023-05-06 00:41:47 -0400

    refactor v2

--- components/Index/CalculateMacros.tsx
+++ components/Index/CalculateMacros.tsx
@@ -10,10 +10,12 @@ const MainPage = styled.div`
   height: 100%;
   padding: 5%;
 `;
+
 const TitleMacros = styled.h2`
   color: #001858;
   font-weight: 700;
 `;
+
 const CalculateMacrosDiv = styled.div`
   margin-top: 40px;
   width: 100%;
