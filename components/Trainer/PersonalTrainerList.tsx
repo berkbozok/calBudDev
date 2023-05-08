d15216dcc1b2a12619b24635eb0c8c64a01d6486
Justin Loh <justinlohkingwei@gmail.com>
2023-05-08 15:48:31 -0400

    Fixed CSS for trainer Page

--- components/Trainer/PersonalTrainerList.tsx
+++ components/Trainer/PersonalTrainerList.tsx
@@ -1,29 +1,30 @@
-import React, { useState } from "react";
+import { TeamOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import styled from "styled-components";
 import { Modal } from "antd";
 import { Card } from "antd";
-import styled from "styled-components";
-import { TeamOutlined } from "@ant-design/icons";
-import { Layout } from "antd";
 
 const { Meta } = Card;
 
-const PersonalTrainerRoot = styled.div`
+const PersonalTrainerListRoot = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: red;
+  background-color: #fef6e4;
+  min-height: 55rem;
 `;
 
-const MainPageLayout = styled.div`
+const PersonalTrainerListHeader = styled.div`
+  display: flex;
+  background-color: #fef6e4;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #596996;
+  padding: 1rem 1rem 1rem 1rem;
+`;
+
+const PersonalTrainerListMainPage = styled.div`
   display: flex;
   flex-direction: row;
-  background-color: #f1ead9;
-  color: #b2b4ba;
-  @media only screen and (max-width: 767px) {
-    .main-page-layout {
-      display: flex;
-      flex-direction: column;
-    }
-  }
 `;
 
 const MainPage = styled.div`
@@ -42,7 +43,7 @@ const ModalAlignment = styled.h2`
   flex-direction: row;
 `;
 
-const PersonalTrainerList = () => {
+function PersonalTrainerList() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedCardIndex, setSelectedCardIndex] = useState(-1);
 
@@ -70,95 +71,88 @@ const PersonalTrainerList = () => {
     },
   ];
 
-  const { Header } = Layout;
-
   return (
-    <>
-      <PersonalTrainerRoot>
-        <Header className="main-title">
-          <TeamOutlined className="icon-title" />
-          Trainers
-        </Header>
-        <MainPageLayout>
-          <MainPage>
-            <TitleTrainer>Find Your Personal Trainer Today</TitleTrainer>
-            <div className="trainer-card">
-              {cards.map((card, index) => (
-                <Card
-                  key={index}
-                  hoverable
-                  className="card"
-                  cover={<img alt="example" src={card.image} />}
-                  onClick={() => showModal(index)}
-                >
-                  <Meta title={card.title} description={card.description} />
-                </Card>
-              ))}
+    <PersonalTrainerListRoot>
+      <PersonalTrainerListHeader>
+        <TeamOutlined className="icon-title" />
+        Trainers
+      </PersonalTrainerListHeader>
+      <PersonalTrainerListMainPage>
+        <MainPage>
+          <TitleTrainer>Find Your Personal Trainer Today</TitleTrainer>
+          <div className="trainer-card">
+            {cards.map((card, index) => (
+              <Card
+                key={index}
+                hoverable
+                className="card"
+                cover={<img alt="example" src={card.image} />}
+                onClick={() => showModal(index)}
+              >
+                <Meta title={card.title} description={card.description} />
+              </Card>
+            ))}
 
-              {selectedCardIndex !== -1 && (
-                <Modal
-                  title={cards[selectedCardIndex].title}
-                  visible={isModalVisible}
-                  onCancel={handleCancel}
-                  className="trainer-modal"
-                  okButtonProps={{
-                    style: { display: "none", width: "500px" },
-                  }}
-                >
-                  <ModalAlignment>
-                    <div>
-                      <p>
-                        Write personal information for{" "}
-                        {cards[selectedCardIndex].title}
-                      </p>
-                      {cards[selectedCardIndex].info}
+            {selectedCardIndex !== -1 && (
+              <Modal
+                title={cards[selectedCardIndex].title}
+                visible={isModalVisible}
+                onCancel={handleCancel}
+                className="trainer-modal"
+                okButtonProps={{
+                  style: { display: "none", width: "500px" },
+                }}
+              >
+                <ModalAlignment>
+                  <div>
+                    <p>
+                      Write personal information for{" "}
+                      {cards[selectedCardIndex].title}
+                    </p>
+                    {cards[selectedCardIndex].info}
 
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>Name:</span> John
-                        Doe
-                      </p>
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>Age:</span> 30
-                      </p>
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>
-                          Years of Experience:
-                        </span>{" "}
-                        5
-                      </p>
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>Phone:</span>{" "}
-                        555-1234
-                      </p>
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>Email:</span>{" "}
-                        johndoe@example.com
-                      </p>
-                      <p>
-                        <span style={{ fontWeight: "bold" }}>
-                          Social Media:
-                        </span>{" "}
-                        @johndoe
-                      </p>
-                    </div>
-                    <div style={{ width: "70%" }}>
-                      <iframe
-                        className="calendly"
-                        src="https://calendly.com/find-trainer/berk"
-                        width="100%"
-                        height="800px"
-                        frameBorder="0"
-                      ></iframe>
-                    </div>
-                  </ModalAlignment>
-                </Modal>
-              )}
-            </div>
-          </MainPage>
-        </MainPageLayout>
-      </PersonalTrainerRoot>
-    </>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>Name:</span> John Doe
+                    </p>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>Age:</span> 30
+                    </p>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>
+                        Years of Experience:
+                      </span>{" "}
+                      5
+                    </p>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>Phone:</span>{" "}
+                      555-1234
+                    </p>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>Email:</span>{" "}
+                      johndoe@example.com
+                    </p>
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>Social Media:</span>{" "}
+                      @johndoe
+                    </p>
+                  </div>
+                  <div style={{ width: "70%" }}>
+                    <iframe
+                      className="calendly"
+                      src="https://calendly.com/find-trainer/berk"
+                      width="100%"
+                      height="800px"
+                      frameBorder="0"
+                    ></iframe>
+                  </div>
+                </ModalAlignment>
+              </Modal>
+            )}
+          </div>
+        </MainPage>
+      </PersonalTrainerListMainPage>
+    </PersonalTrainerListRoot>
   );
-};
+}
 
 export default PersonalTrainerList;
