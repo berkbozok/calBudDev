e0d00820b14bf59f005ab9ab1b94d211001294b7
berkbozok <berk_bozok@live.com>
2023-05-15 23:48:59 -0400

    home edit

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Button, Modal } from "antd";
-import { Card } from "antd";
 import Link from "next/link";
 import Logo from "../../shared/logo";
 
