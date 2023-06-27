f910ac9e8088c787d298dca6bf840823c14ae80a
berkbozok <berk_bozok@live.com>
2023-06-27 00:06:51 -0400

    option styling improved

--- components/MealPlans/MealPlanner.tsx
+++ components/MealPlans/MealPlanner.tsx
@@ -107,7 +107,6 @@ const MealPlanner = () => {
 
     let mealPlanReport = "Meal Plan Report:\n";
     mealPlanReport += "------------------\n";
-
     const mealTimes: { [key: string]: string[] } = {
       Breakfast: [],
       Lunch: [],
