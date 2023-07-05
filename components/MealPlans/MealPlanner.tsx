2c3adfdb7d8ecfd9e562a693c35e679e3bff311f
berkcoeurway <berk.bozok@coeurway.com>
2023-07-05 08:15:23 -0400

    meals adjusted

--- components/MealPlans/MealPlanner.tsx
+++ components/MealPlans/MealPlanner.tsx
@@ -104,7 +104,6 @@ const MealPlanner = () => {
       const mealPortionSize = desiredCalories / totalCalories;
       adjustedPortionSizes[meal] = mealPortionSize;
     });
-
     let mealPlanReport = "Meal Plan Report:\n";
     mealPlanReport += "------------------\n";
 
