898485635aea0e0c93af82fc474f3722e1c8d806
berkcoeurway <berk.bozok@coeurway.com>
2023-07-12 10:55:59 -0400

    visual update

--- components/MealPlans/MealPlanner.tsx
+++ components/MealPlans/MealPlanner.tsx
@@ -108,7 +108,6 @@ const MealPlanner = () => {
     let mealPlanReport = "Meal Plan Report:\n";
 
     mealPlanReport += "------------------\n";
-
     const mealTimes: { [key: string]: string[] } = {
       Breakfast: [],
       Lunch: [],
