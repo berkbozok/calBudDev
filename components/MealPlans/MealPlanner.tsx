6dd89850fceda5bd8d4dc1c4bdafc4f11fb6ef73
berkbozok <berk_bozok@live.com>
2023-07-17 03:27:59 -0400

    commit test

--- components/MealPlans/MealPlanner.tsx
+++ components/MealPlans/MealPlanner.tsx
@@ -108,6 +108,7 @@ const MealPlanner = () => {
     let mealPlanReport = "Meal Plan Report:\n";
 
     mealPlanReport += "------------------\n";
+
     const mealTimes: { [key: string]: string[] } = {
       Breakfast: [],
       Lunch: [],
