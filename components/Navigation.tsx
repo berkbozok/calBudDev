3ff64ea1c50532e4e416ebc9ab22aec072f6c06c
berkbozok <berk_bozok@live.com>
2023-08-11 23:59:27 -0400

    tailwind initialized

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -152,7 +152,6 @@ export default function Navigation() {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
